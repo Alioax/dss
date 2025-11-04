21cc41a57c5ceb56f67a6574dadf554b5b064247
Ali Haghighi <alihghila@gmail.com>
2025-11-05 00:27:05 +0330

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 // next.config.mjs
-const isProd = process.env.NODE_ENV === "production";
-
-// If repo is https://alioax.github.io/dss -> set to "dss"
-// If repo is https://alioax.github.io       -> set to ""
-const repo = "dss"; // <— REPLACE THIS
 
 export default {
-  // output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: isProd && repo ? `/${repo}` : "",
-  assetPrefix: isProd && repo ? `/${repo}/` : "",
 };
